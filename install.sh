07e83a24e1957aa28d4b4d4c44904ad7af773b26
Tim Schindler <tim@catalyst-zero.com>
2014-02-14 12:04:26 +0100

    test

--- install.sh
+++ install.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
-echo "#### link dotfiles"
+PWD=$(pwd)
+HOME=${$PWD:26}
 
-# bash profile
-ln -sf ~/projects/private/dotfiles/.bash_profile ~/.bash_profile
-
-# git config
-ln -sf ~/projects/private/dotfiles/.gitconfig ~/.gitconfig
-
-# vim
 # update vi on ubuntu to get vim plugins to work
+echo "#### install vi"
 if [ "$(uname)" != "Darwin" ]; then
    sudo apt-get install -y vim
 fi
 
-rm -rf ~/.vim
-rm -rf ~/projects/private/dotfiles/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/projects/private/dotfiles/.vim/bundle/vundle
-ln -s ~/projects/private/dotfiles/.vim ~
-ln -sf ~/projects/private/dotfiles/.vimrc ~/.vimrc
+echo "#### link dotfiles"
+
+rm -rf "$HOME/.bash_profile"
+ln -sf "$PWD/.bash_profile" "$HOME/.bash_profile"
+
+rm -rf "$HOME/.gitconfig"
+ln -sf "$PWD/.gitconfig" "$HOME/.gitconfig"
+
+rm -rf "$HOME/.vim"
+ln -s "$HOME/.vim" "$HOME/.vim"
+
+rm -rf "$HOME/.vimrc"
+ln -sf "$HOME/.vimrc" "$HOME/.vimrc"
+
+rm -rf "$PWD/.vim/bundle"
+git clone https://github.com/gmarik/vundle.git "$PWD/.vim/bundle/vundle"
 
 echo "#### install vim plugins"
 
 vim +BundleInstall +qall
 
-source ~/projects/private/dotfiles/.bash_profile
+source "$PWD/.bash_profile"
