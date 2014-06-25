3596f811b2e4c9adfead014feb7b1938a1084a1a
Tim Schindler <tim@catalyst-zero.com>
2014-06-25 07:53:47 +0200

    changed vagrant shell provisioner

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -14,11 +14,12 @@ sh "${HOME}/projects/private/dotfiles/install.sh" $HOME
 chown -R "${USER}:${USER}" $HOME
 
 # install go
-wget --no-verbose https://go.googlecode.com/files/go1.2.src.tar.gz
-tar -v -C /usr/local -xzf go1.2.src.tar.gz > /dev/null
+GO_VERSION="1.3"
+wget --no-verbose http://golang.org/dl/go${GO_VERSION}.src.tar.gz
+tar -v -C /usr/local -xzf go${GO_VERSION}.src.tar.gz > /dev/null
 cd /usr/local/go/src && ./make.bash --no-clean 2>&1 > /dev/null
 echo "export PATH=$PATH:/usr/local/go/bin" > /etc/bash.bashrc
-rm go1.2.src.tar.gz
+rm go${GO_VERSION}.src.tar.gz
 
 # update docker
 curl -s https://get.docker.io/ubuntu/ | sudo sh
@@ -29,3 +30,19 @@ service docker restart
 
 # set default editor
 update-alternatives --set editor /usr/bin/vim.basic
+
+# install aws cli
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install awscli
+
+# install cloudflare cli
+curl https://raw.githubusercontent.com/creationix/nvm/v0.8.0/install.sh | sh
+nvm install v0.10.24
+nvm alias default 0.10.24
+npm install cloudflare-cli -g
+
+# install jq
+wget http://stedolan.github.io/jq/download/linux64/jq
+chmod +q jq
+mv jq /usr/local/bin/
