c00a4cf1666bc574a32eb2ee38ac4a73bf534f98
Tim Schindler <tim@giantswarm.io>
2015-02-07 02:33:28 +0000

    install zprezto

--- install.sh
+++ install.sh
@@ -28,7 +28,9 @@ ln -sv "${WD}/.vim" "${HOME}/.vim"
 rm -rf "${HOME}/.vimrc"
 ln -sfv "${WD}/.vimrc" "${HOME}/.vimrc"
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
+rm -rf "${HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${WD}/.zprezto"
+ln -sv "${WD}/.zprezto" "${HOME}/.zprezto/runcoms/zprezto"
 
 rm -rf "${HOME}/.zlogin"
 ln -sv "${WD}/.zlogin" "${HOME}/.zprezto/runcoms/zlogin"
