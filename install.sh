e94d8e3bc7fb283dea77132c687adc42e5613f56
Tim Schindler <tim@giantswarm.io>
2015-08-28 17:35:40 +0000

    clean zprezto

--- install.sh
+++ install.sh
@@ -28,11 +28,11 @@ ln -sv "${WD}/.vim" "${HOME}/.vim"
 rm -rf "${HOME}/.vimrc"
 ln -sfv "${WD}/.vimrc" "${HOME}/.vimrc"
 
-rm -rf "${WD}/.vim/bundle"
-git clone https://github.com/VundleVim/Vundle.vim.git "${WD}/.vim/bundle/Vundle.vim"
-
 echo
 echo "#### install vim plugins"
+
+rm -rf "${WD}/.vim/bundle/*"
+git clone https://github.com/VundleVim/Vundle.vim.git "${WD}/.vim/bundle/Vundle.vim"
 
 vim +BundleInstall +qall > /dev/null
 cp -rf "${WD}/snippets/go.snippets" "${HOME}/.vim/bundle/vim-snippets/snippets/go.snippets"
@@ -43,21 +43,3 @@ echo "#### install zprezto"
 rm -rf "${HOME}/.zprezto"
 git clone --recursive https://github.com/zyndiecate/prezto.git "${WD}/.zprezto"
 ln -sv "${WD}/.zprezto" "${HOME}/.zprezto"
-
-rm -rf "${HOME}/.zlogin"
-ln -sv "${WD}/.zprezto/runcoms/zlogin" "${HOME}/.zlogin"
-
-rm -rf "${HOME}/.zlogout"
-ln -sv "${WD}/.zprezto/runcoms/zlogout" "${HOME}/.zlogout"
-
-rm -rf "${HOME}/.zpreztorc"
-ln -sv "${WD}/.zprezto/runcoms/zpreztorc" "${HOME}/.zpreztorc"
-
-rm -rf "${HOME}/.zprofile"
-ln -sv "${WD}/.zprezto/runcoms/zprofile" "${HOME}/.zprofile"
-
-rm -rf "${HOME}/.zshenv"
-ln -sv "${WD}/.zprezto/runcoms/zshenv" "${HOME}/.zshenv"
-
-rm -rf "${HOME}/.zshrc"
-ln -sv "${WD}/.zprezto/runcoms/zshrc" "${HOME}/.zshrc"
